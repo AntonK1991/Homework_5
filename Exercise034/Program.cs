90cd131c53886bc6f0a2f7c3b2944bf9d1ac69ca
Anton <antonkononoff@yandex.ru>
2022-11-07 21:55:04 +0500

    Init

---
+++ Exercise034/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+
+int[] CreateMassive(int n)
+{
+    int[] array = new int[n];
+    return (array);
+}
+
+int[] RandomFillMassive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return (array);
+}
+
+int [] PrintMassive(int[] array)
+{
+    System.Console.WriteLine ("Элементы массива: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]+" ");
+    }
+    System.Console.WriteLine ();
+    return array;
+}
+
+void NumsCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine("Количество четных элементов в массиве - " + count);
+}
+
+Console.Write("Введите количество элементов массива: ");
+int input = Convert.ToInt32(Console.ReadLine());
+NumsCount(PrintMassive(RandomFillMassive(CreateMassive(input))));
+
---
+++ Exercise036/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int Input()
+{
+    Console.Write("Введите количество элементов массива: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int[] CreateMassive(int n)
+{
+    int[] array = new int[n];
+    return (array);
+}
+
+int[] RandomFillMassive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    return (array);
+}
+
+int[] PrintMassive(int[] array)
+{
+    System.Console.WriteLine("Элементы массива: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+    return array;
+}
+
+void OddIndexSumm(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            summ = summ + array[i];
+        }
+    }
+    Console.WriteLine("Cумма элементов, стоящих на нечётных позициях - " + summ);
+}
+
+OddIndexSumm(PrintMassive(RandomFillMassive(CreateMassive(Input()))));
+
